96513c4978967be58dde0047625298de00252382
LSCHOTT <lilian.schott@ynov.com>
2023-04-27 11:36:05 +0200

    lives management added

--- src/Hangman.java
+++ src/Hangman.java
@@ -4,7 +4,7 @@ public class Hangman {
 	private String WordToFind;
 	private String CurrentWord;
 	private boolean IsWordFind;
-	
+	private int Lives=10;
 	public Hangman(String wordToFind) {
 		this.WordToFind = wordToFind;
 		this.CurrentWord = "";
@@ -18,5 +18,21 @@ public class Hangman {
 	public String toString() {
 		return this.WordToFind + ", " + this.CurrentWord + ", " + this.IsWordFind;
 	}
-	
+	public void Wrongletter(int Lives) {
+		System.out.println(Lives);
+		System.out.println("Wrong letter! You lost 1 life");
+		Lives-=1;
+		System.out.println(Lives);
+	}
+	public void Wrongword(int Lives) {
+		System.out.println(Lives);
+		System.out.println("Wrong letter! You lost 1 life");
+		Lives-=2;
+		System.out.println(Lives);
+	}
+	public void GameOver(int Lives) {
+		System.out.println(Lives);
+		System.out.println("Game over");
+		System.exit(0);
+	}
 }
