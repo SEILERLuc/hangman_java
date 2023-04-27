6d1c9e02720f0edfcb4f9a6679e8cfb653413267
LSEILER <luc.seiler@ynov.com>
2023-04-27 11:43:50 +0200

    Add informations to the main

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,23 @@
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//Choose a random word for the Hangman
 		mot word=new mot();
 		word.ChooseRandomWord();
 		System.out.println("HANGMAN en java");
 		
+		//Create the Player
+		Player player = new Player();
+		
+		//Create a Hangman
 		Hangman hangman = new Hangman("HELLOWORLD");
 		System.out.println(hangman.toString());
+		
+		// Faire menu
+		// afficher le mot en tirets
+		// demander à l'utilisateur de saisir une entrée
+		// se servir des autres classes pour détecter la présence du mot/de la lettre
+		// réafficher le mot en tirets mis à jour
 	}
 
 }
