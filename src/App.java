036606be175d584a5085799523309a4343cf0b78
LSCHOTT <lilian.schott@ynov.com>
2023-05-04 11:36:32 +0200

    menu can be played in loop

--- src/App.java
+++ src/App.java
@@ -1,7 +1,6 @@
 import java.util.Scanner;
 
 public class App {
-
 	private String name;
 	String[] wordList = {
 			"banc",
@@ -65,63 +64,67 @@ public class App {
 		
 		// Hangman
 		Hangman hangman = new Hangman(this.getRandomWord());
-		System.out.println(hangman.toString());
-		hangman.displayWord();
 		
 		/* --- MENU --- */
 		while (true) {
-			System.out.println("Voici la liste des options :");
-			System.out.println("1- Saisir une lettre (-1 si échec)");
-			System.out.println("2- Saisir un mot (-2 si échec)");
-			System.out.println("3- Quitter sans sauvegarder");
-			System.out.println("4- Quitter en sauvegardant");
-				
-			Scanner scanner = new Scanner(System.in);
-			System.out.print("Entrez votre choix : ");
-			String userChoice = scanner.nextLine();
-			System.out.println("Vous avez entré : " + userChoice);
-			
-			if (userChoice.equals("1")) {
-				//scanner.close();
-			
-				Scanner scannerLetter = new Scanner(System.in);
+			if(hangman.GetLives()>0) {
+				System.out.println("Voici la liste des options :");
+				System.out.println("1- Saisir une lettre (-1 si échec)");
+				System.out.println("2- Saisir un mot (-2 si échec)");
+				System.out.println("3- Quitter sans sauvegarder");
+				System.out.println("4- Quitter en sauvegardant");
+					
+				Scanner scanner = new Scanner(System.in);
 				System.out.print("Entrez votre choix : ");
-				String userLetter = scannerLetter.nextLine();
-				System.out.println("Vous avez entré : " + userLetter);
-				player.setLetter(userLetter);
-				System.out.println("Letter of the player " + player.getLetter());
-				if (hangman.isInside(userLetter)) {
-					System.out.println("La lettre choisie est dans le mot");
-				} else {
-					System.out.println("La lettre choisie n'est pas dans le mot");
-					hangman.Wrongletter();
+				String userChoice = scanner.nextLine();
+				System.out.println("Vous avez entré : " + userChoice);
+				
+				if (userChoice.equals("1")) {
+					//scanner.close();
+				
+					Scanner scannerLetter = new Scanner(System.in);
+					System.out.print("Entrez votre choix : ");
+					String userLetter = scannerLetter.nextLine();
+					System.out.println("Vous avez entré : " + userLetter);
+					player.setLetter(userLetter);
+					System.out.println("Letter of the player " + player.getLetter());
+					if (hangman.isInside(userLetter)) {
+						System.out.println("La lettre choisie est dans le mot");
+					} else {
+						System.out.println("La lettre choisie n'est pas dans le mot");
+						hangman.Wrongletter();
+					}
+					//scannerLetter.close();
 				}
-				//scannerLetter.close();
-				break;
-			}
-			if (userChoice.equals("2")) {
+				if (userChoice.equals("2")) {
+					//scanner.close();
+				
+					Scanner scannerWord = new Scanner(System.in);
+					System.out.println("Entrez votre choix : ");
+					String userWord = scannerWord.nextLine();
+					System.out.println("Vous avez entré : " + userWord);
+					player.setLetter(userWord);
+					System.out.println("Word of the player " + player.getWord());
+					if (userWord.equals(hangman.getWordToFind())) {
+						System.out.println("Le mot choisi est  correct");
+						hangman.Win();
+					} else {
+						System.out.println("Le mot choisi est incorrect");
+						hangman.Wrongword();
+					}
+					//scannerLetter.close();
+				}
 				//scanner.close();
-			
-				Scanner scannerWord = new Scanner(System.in);
-				System.out.println("Entrez votre choix : ");
-				String userWord = scannerWord.nextLine();
-				System.out.println("Vous avez entré : " + userWord);
-				player.setLetter(userWord);
-				System.out.println("Word of the player " + player.getWord());
-				if (userWord.equals(hangman.getWordToFind())) {
-					System.out.println("Le mot choisi est  correct");
-					hangman.Win();
-				} else {
-					System.out.println("Le mot choisi est incorrect");
-					hangman.Wrongword();
+				
+				
 				}
-				//scannerLetter.close();
-				break;
-			}
-			//scanner.close();
+		if(hangman.GetLives()==0) {
+			hangman.GameOver();
 		}
-		System.out.println("FIN");
-	}
+		}
+		
+			}
+	
 	
 	public void generateMenu() {
 		Menu menu = new Menu("MyMenu");
